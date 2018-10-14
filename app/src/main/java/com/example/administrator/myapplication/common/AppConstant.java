9cb8ab7ed20fed455d01f033f6b48234108c89c3
Danchaofan <215270328@qq.com>
2018-10-14 17:28:36 +0800

    第一次提交

---
+++ app/src/main/java/com/example/administrator/myapplication/common/AppConstant.java
@@ -0,0 +1,14 @@
+package com.example.administrator.myapplication.common;
+
+/**
+ * Created by asus on 2016/8/28.
+ */
+public class AppConstant {
+
+    /**
+     * SharePreference存储路径
+     */
+    public static final String SHARE_PATH = "CoolShopping";
+
+
+}
---
+++ app/src/main/java/com/example/administrator/myapplication/main/MainRecycleView.java
@@ -0,0 +1,8 @@
+package com.example.administrator.myapplication.main;
+
+/**
+ * Created by Administrator on 2018/10/14.
+ */
+
+public class MainRecycleView {
+}
